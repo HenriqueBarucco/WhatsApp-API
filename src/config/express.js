78a596c43145a98f9f516d767aa0f2a924512a10
Henrique Barucco <henrique_barucco@hotmail.com>
2022-12-29 11:04:08 -0300

    fix CORS

--- src/config/express.js
+++ src/config/express.js
@@ -8,6 +8,7 @@ const error = require('../api/middlewares/error')
 const tokenCheck = require('../api/middlewares/tokenCheck')
 const { protectRoutes } = require('./config')
 
+app.use(cors());
 app.use(express.json())
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true }))
@@ -21,19 +22,5 @@ if (protectRoutes) {
 }
 app.use('/', routes)
 app.use(error.handler)
-
-const allowedOrigins = ['https://projetos.grupocriar.com.br'];
-
-app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-
-}));
 
 module.exports = app
