b162173960831bfcb9d7ed05a70f6044342922ae
Henrique Barucco <33501527+HenriqueBarucco@users.noreply.github.com>
2023-03-09 00:22:45 +0000

    eslint: fix

--- src/api/class/instance.js
+++ src/api/class/instance.js
@@ -545,7 +545,7 @@ class WhatsAppInstance {
                 footer: data.footerText ?? '',
             },
             {
-                ephemeralExpiration: 604800
+                ephemeralExpiration: 604800,
             }
         )
         return result
